01a289c8948b6e46333d820f622e0353ec15eeb2
YoungB <brr23@njit.edu>
2019-05-16 12:06:20 -0400

    Database setup for Heroku

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-05-16 15:02:48.
+    * Generated on 2019-05-16 15:54:38.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
